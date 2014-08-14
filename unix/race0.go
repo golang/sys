0c07981137ab98dfa9d09e66f2dfe7de49ae8904
Rob Pike <r@golang.org>
2014-08-13 20:54:29 -0700

    go.sys/unix: add asm hooks for solaris to runtime-resident syscall help also: race is supported on freebsd, so fix a couple of build tags
    LGTM=minux
    R=rsc, minux
    CC=golang-codereviews
    https://golang.org/cl/124330043

--- unix/race0.go
+++ unix/race0.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin,!race dragonfly,!race freebsd linux netbsd openbsd solaris
+// +build darwin,!race linux,!race freebsd,!race netbsd openbsd solaris dragonfly
 
 package unix
 
