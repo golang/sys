96cf2ceb2c3821511d200643cb559e81cef26e40
Rob Pike <r@golang.org>
2014-08-14 17:39:12 -0700

    go.sys/unix: arm uses B not JMP Fix build.
    LGTM=rsc
    R=rsc
    CC=golang-codereviews
    https://golang.org/cl/123510043

--- unix/asm_freebsd_arm.s
+++ unix/asm_freebsd_arm.s
@@ -12,16 +12,16 @@
 // The runtime may know about them.
 
 TEXT	·Syscall(SB),NOSPLIT,$0-28
-	JMP	syscall·Syscall(SB)
+	B	syscall·Syscall(SB)
 
 TEXT	·Syscall6(SB),NOSPLIT,$0-40
-	JMP	syscall·Syscall6(SB)
+	B	syscall·Syscall6(SB)
 
 TEXT	·Syscall9(SB),NOSPLIT,$0-52
-	JMP	syscall·Syscall9(SB)
+	B	syscall·Syscall9(SB)
 
 TEXT	·RawSyscall(SB),NOSPLIT,$0-28
-	JMP	syscall·RawSyscall(SB)
+	B	syscall·RawSyscall(SB)
 
 TEXT	·RawSyscall6(SB),NOSPLIT,$0-40
-	JMP	syscall·RawSyscall6(SB)
+	B	syscall·RawSyscall6(SB)
--- unix/asm_linux_arm.s
+++ unix/asm_linux_arm.s
@@ -12,16 +12,16 @@
 // The runtime may know about them.
 
 TEXT	·Syscall(SB),NOSPLIT,$0-28
-	JMP	syscall·Syscall(SB)
+	B	syscall·Syscall(SB)
 
 TEXT	·Syscall6(SB),NOSPLIT,$0-40
-	JMP	syscall·Syscall6(SB)
+	B	syscall·Syscall6(SB)
 
 TEXT ·RawSyscall(SB),NOSPLIT,$0-28
-	JMP	syscall·RawSyscall(SB)
+	B	syscall·RawSyscall(SB)
 
 TEXT	·RawSyscall6(SB),NOSPLIT,$0-40
-	JMP	syscall·RawSyscall6(SB)
+	B	syscall·RawSyscall6(SB)
 
 TEXT ·seek(SB),NOSPLIT,$0-32
-	JMP	syscall·seek(SB)
+	B	syscall·seek(SB)
