7d091bfd99a86b1bf198194a0b5bf81164f01c12
Alex Brainman <alex.brainman@gmail.com>
2014-08-19 15:20:17 +1000

    go.sys/windows: use $GOROOT/src/pkg/syscall/mksyscall_windows.go to generate syscalls
    Also zsyscall_windows_386.go and zsyscall_windows_amd64.go
    have same contents, so merge them into single file.
    
    LGTM=r
    R=r
    CC=golang-codereviews
    https://golang.org/cl/124430043

--- windows/mkall_windows.bat
+++ windows/mkall_windows.bat
@@ -3,19 +3,13 @@
 :: license that can be found in the LICENSE file.
 @echo off
 
-if exist mkall.sh goto dirok
-echo mkall_windows.bat must be run from src\pkg\syscall directory
+if exist syscall_windows.go goto dirok
+echo mkall_windows.bat must be run from go.sys\windows directory
 goto :end
 :dirok
 
-if "%1"=="386" goto :paramok
-if "%1"=="amd64" goto :paramok
-echo parameters must be 386 or amd64
-goto :end
-:paramok
-
-go build mksyscall_windows.go
-.\mksyscall_windows syscall_windows.go security_windows.go syscall_windows_%1.go |gofmt >zsyscall_windows_%1.go
+go build -o mksyscall_windows.exe %GOROOT%\src\pkg\syscall\mksyscall_windows.go
+mksyscall_windows.exe syscall_windows.go security_windows.go |gofmt >zsyscall_windows.go
 del mksyscall_windows.exe
 
 :end
