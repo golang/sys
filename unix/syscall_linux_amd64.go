04f0f96bfaf771fb288e160e8488fb96562ee3e0
Ian Lance Taylor <iant@golang.org>
2015-05-13 15:12:00 -0700

    unix: move InotifyInit into GOARCH specific Linux files
    This is a copy of http://golang.org/cl/9870 in the syscall package.
    
    This is in preparation for arm64 support, as the arm64 Linux kernel
    does not have an inotify_init system call, only inotify_init1.
    
    Update golang/go#10150.
    
    Change-Id: I9224a094af79adbb2f5714ad977b195d5a941eb0
    Reviewed-on: https://go-review.googlesource.com/10036
    Reviewed-by: Rob Pike <r@golang.org>

--- unix/syscall_linux_amd64.go
+++ unix/syscall_linux_amd64.go
@@ -17,6 +17,7 @@ import "syscall"
 //sysnb	Getgid() (gid int)
 //sysnb	Getrlimit(resource int, rlim *Rlimit) (err error)
 //sysnb	Getuid() (uid int)
+//sysnb	InotifyInit() (fd int, err error)
 //sys	Ioperm(from int, num int, on int) (err error)
 //sys	Iopl(level int) (err error)
 //sys	Lchown(path string, uid int, gid int) (err error)
