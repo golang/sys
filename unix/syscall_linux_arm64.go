62bee037599929a6e9146f29d10dd5208c43507d
kortschak <dan.kortschak@adelaide.edu.au>
2016-06-15 08:22:37 +0930

    unix: fix Ppoll for arm64
    Fixes golang/go#16065.
    
    Change-Id: I97d06b7038e7c7507e765736899cfa243f45805c
    Reviewed-on: https://go-review.googlesource.com/24121
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- unix/syscall_linux_arm64.go
+++ unix/syscall_linux_arm64.go
@@ -182,8 +182,8 @@ const (
 func Poll(fds []PollFd, timeout int) (n int, err error) {
 	var ts *Timespec
 	if timeout >= 0 {
-		ts = new(*Timespec)
-		*ts = NsecToTimespec(timeout * 1e6)
+		ts = new(Timespec)
+		*ts = NsecToTimespec(int64(timeout) * 1e6)
 	}
 	if len(fds) == 0 {
 		return ppoll(nil, 0, ts, nil)
