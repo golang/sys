f0d39a4bd5f1a0a15ef46503d12cffc780be4b9b
Tobias Klauser <tklauser@distanz.ch>
2017-10-25 14:20:02 +0200

    unix: add Poll function on Darwin
    Tested with TestPoll extracted from syscall_linux_test.go. Once Poll is
    supported on all OSes this test can be moved to syscall_unix_test.go.
    
    Change-Id: Ia57e3a92e6a50cb22edde8a31e1c4e70d894dc4c
    Reviewed-on: https://go-review.googlesource.com/73310
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- unix/syscall_darwin.go
+++ unix/syscall_darwin.go
@@ -239,6 +239,15 @@ func IoctlGetTermios(fd int, req uint) (*Termios, error) {
 	return &value, err
 }
 
+//sys	poll(fds *PollFd, nfds int, timeout int) (n int, err error)
+
+func Poll(fds []PollFd, timeout int) (n int, err error) {
+	if len(fds) == 0 {
+		return poll(nil, 0, timeout)
+	}
+	return poll(&fds[0], len(fds), timeout)
+}
+
 /*
  * Exposed directly
  */
@@ -377,7 +386,6 @@ func IoctlGetTermios(fd int, req uint) (*Termios, error) {
 // Searchfs
 // Delete
 // Copyfile
-// Poll
 // Watchevent
 // Waitevent
 // Modwatch
