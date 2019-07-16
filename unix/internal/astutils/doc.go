603c9c2aa780338546c18eb9d5537af2e5b9474b
Pierre Curto <pierre.curto@gmail.com>
2019-07-16 15:11:19 +0200

    added Go authors header to all files

--- unix/internal/astutils/doc.go
+++ unix/internal/astutils/doc.go
@@ -1,3 +1,7 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package astutils provides helpers to manipulate go/ast objects.
 //
 // All the *Equal functions follow pointers but ignore positions and comments.
