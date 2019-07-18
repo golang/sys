97f774859591763725bd1537aaec9517900c0d73
Pierre Curto <pierre.curto@gmail.com>
2019-07-18 18:31:39 +0200

    internal/merge: added package description

---
+++ unix/internal/merge/doc.go
@@ -0,0 +1,22 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package merge is used to consolidate constants, types and functions
+// from the sys generated files grouping them by GOOS and GOARCH.
+// The files named as <name>_<goos>_<goarch>.go are processed and the consolidated
+// objects are stored in a new file named <name>_<goos>.go.
+//
+// The Registry is used to collect all the files data, agggregate the objects and
+// output the new files.
+// It works as follow:
+//  - parse the sys package only selecting the files we are interested in
+//  - group the files by name and for each name by GOOS and GOARCH
+//    (cf. the Registry type)
+//  - build the list of constants, types and functions per file (GOOS/GOARCH)
+//  - consolidate the objects: they must all be present in the related GOOS/GOARCH files
+//    (intersection of all the constants, types and functions)
+//  - remove the consolidated objects from each GOOS/GOARCG file
+//  - update the input files with their stripped down version
+//  - create the new files containing the consolidated objects
+package merge
