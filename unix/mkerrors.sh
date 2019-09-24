e2fefa8ec2a940fb6ef4eb6e2f06905b6d9d296b
Tobias Klauser <tklauser@distanz.ch>
2019-09-24 09:34:00 +0000

    unix: fix Select to return number of ready fds on Solaris
    Make Select's signature on Solaris match the one on Linux and return the number
    of ready file descriptors.
    
    Updates golang/go#34458
    
    Change-Id: I118c4c35cbc83dba015ef357ce9bef44c9165ec1
    Reviewed-on: https://go-review.googlesource.com/c/sys/+/196807
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- unix/mkerrors.sh
+++ unix/mkerrors.sh
@@ -348,6 +348,7 @@ includes_OpenBSD='
 includes_SunOS='
 #include <limits.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/sockio.h>
 #include <sys/stat.h>
