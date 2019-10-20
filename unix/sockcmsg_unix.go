3e7259c5e7c2076bb2728047a3df75adb1bad8e5
Tobias Klauser <tklauser@distanz.ch>
2019-10-20 22:30:17 +0200

    unix: don't use 32-bit aligned access for cmsgAlignOf on dragonfly after ABI change
    Use 32-bit alignment for versions before the September 2019 ABI changes
    http://lists.dragonflybsd.org/pipermail/users/2019-September/358280.html
    
    Follows CL 201977 which did the same for package syscall.
    
    Updates golang/go#34958
    
    Change-Id: I0e13fccf6563e4d34dd4aa7410be044881f220aa
    Reviewed-on: https://go-review.googlesource.com/c/sys/+/202179
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- unix/sockcmsg_unix.go
+++ unix/sockcmsg_unix.go
@@ -9,34 +9,8 @@
 package unix
 
 import (
-	"runtime"
 	"unsafe"
 )
-
-// Round the length of a raw sockaddr up to align it properly.
-func cmsgAlignOf(salen int) int {
-	salign := SizeofPtr
-
-	switch runtime.GOOS {
-	case "aix":
-		// There is no alignment on AIX.
-		salign = 1
-	case "darwin", "dragonfly", "solaris", "illumos":
-		// NOTE: It seems like 64-bit Darwin, DragonFly BSD,
-		// illumos, and Solaris kernels still require 32-bit
-		// aligned access to network subsystem.
-		if SizeofPtr == 8 {
-			salign = 4
-		}
-	case "netbsd", "openbsd":
-		// NetBSD and OpenBSD armv7 require 64-bit alignment.
-		if runtime.GOARCH == "arm" {
-			salign = 8
-		}
-	}
-
-	return (salen + salign - 1) & ^(salign - 1)
-}
 
 // CmsgLen returns the value to store in the Len field of the Cmsghdr
 // structure, taking into account any necessary alignment.
