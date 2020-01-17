f4af40b0c310a5084eba4e2391afd85e3633a44c
Doorer <zzjialing@gmail.com>
2020-01-17 13:42:39 +0800

    fix mips64le missing InotifyInit
    Signed-off-by: Doorer <zzjialing@gmail.com>

--- unix/syscall_linux_mips64x.go
+++ unix/syscall_linux_mips64x.go
@@ -224,3 +224,14 @@ func Poll(fds []PollFd, timeout int) (n int, err error) {
 	}
 	return poll(&fds[0], len(fds), timeout)
 }
+
+// Fix mips64le missing InotifyInit
+
+func InotifyInit() (fd int, err error) {
+       r0, _, e1 := RawSyscall(SYS_INOTIFY_INIT, 0, 0, 0)
+       fd = int(r0)
+       if e1 != 0 {
+               err = errnoErr(e1)
+       }
+       return
+}
