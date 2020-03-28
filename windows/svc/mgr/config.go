033a5e1701f148e7bfa4d223defd34321fb3e8bf
Yaroslav Vorobiov <yar.vorobiov@gmail.com>
2020-03-28 17:12:50 +0200

    windows/svc: fix manager config toStringSlice pointer casting

--- windows/svc/mgr/config.go
+++ windows/svc/mgr/config.go
@@ -8,7 +8,6 @@ package mgr
 
 import (
 	"syscall"
-	"unicode/utf16"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
@@ -51,20 +50,23 @@ func toString(p *uint16) string {
 }
 
 func toStringSlice(ps *uint16) []string {
-	if ps == nil {
-		return nil
-	}
 	r := make([]string, 0)
-	for from, i, p := 0, 0, (*[1 << 24]uint16)(unsafe.Pointer(ps)); true; i++ {
-		if p[i] == 0 {
-			// empty string marks the end
-			if i <= from {
-				break
-			}
-			r = append(r, string(utf16.Decode(p[from:i])))
-			from = i + 1
-		}
+	p := unsafe.Pointer(ps)
+
+	offset := func(count int) uintptr {
+		return unsafe.Sizeof(uint16(0)) * (uintptr)(count)
 	}
+
+	for {
+		s := windows.UTF16PtrToString((*uint16)(p), 4096)
+		if s == "" {
+			break
+		}
+
+		r = append(r, s)
+		p = unsafe.Pointer(uintptr(p) + offset(len(s)+1))
+	}
+
 	return r
 }
 
