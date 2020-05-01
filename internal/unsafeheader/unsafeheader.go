bc7a7d42d5c30f4d0fe808715c002826ce2c624e
Tobias Klauser <tklauser@distanz.ch>
2020-05-01 16:24:05 +0200

    internal/unsafeheader: fix typos in comments
    The typos in the package godoc comment were pointed out in CL 231223
    which added similar functionality to the main Go repository. Also
    correct the name in the godoc comment for type String.
    
    Change-Id: I69910f3e24b1222190e1adf7fc3d425f0006f96e
    Reviewed-on: https://go-review.googlesource.com/c/sys/+/231517
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Bryan C. Mills <bcmills@google.com>

--- internal/unsafeheader/unsafeheader.go
+++ internal/unsafeheader/unsafeheader.go
@@ -3,10 +3,10 @@
 // license that can be found in the LICENSE file.
 
 // Package unsafeheader contains header declarations for the Go runtime's
-// slice and struct implementations.
+// slice and string implementations.
 //
 // This package allows x/sys to use types equivalent to
-// reflect.SliceHeader and reflect.StructHeader without introducing
+// reflect.SliceHeader and reflect.StringHeader without introducing
 // a dependency on the (relatively heavy) "reflect" package.
 package unsafeheader
 
@@ -22,7 +22,7 @@ type Slice struct {
 	Cap  int
 }
 
-// StringHeader is the runtime representation of a string.
+// String is the runtime representation of a string.
 // It cannot be used safely or portably and its representation may change in a later release.
 type String struct {
 	Data unsafe.Pointer
