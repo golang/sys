golang/sys

commit 765f4ea38db36397e827c4153018aa272eed7835

unix: update PERF consts on linux
Update the PERF_* consts from /usr/include/linux/perf_event.h.

Change-Id: I2dfa09445547682fa7ed99b385476319e8493c27
Reviewed-on: https://go-review.googlesource.com/c/sys/+/259477
Trust: Ian Lance Taylor <iant@golang.org>
Run-TryBot: Ian Lance Taylor <iant@golang.org>
TryBot-Result: Go Bot <gobot@golang.org>
Reviewed-by: Tobias Klauser <tobias.klauser@gmail.com>
Reviewed-by: Matt Layher <mdlayher@gmail.com>

unix/linux/types.go
@@ -910,8 +910,6 @@ const _C__NSIG = C._NSIG
 
 type SignalfdSiginfo C.struct_signalfd_siginfo
 
-const PERF_IOC_FLAG_GROUP = C.PERF_IOC_FLAG_GROUP
-
 // Terminal handling
 
 type Termios C.termios_t
@@ -1038,14 +1036,16 @@ const (
 	PerfBitContextSwitch                 = CBitFieldMaskBit26
 )
 
+// generated by:
+// perl -nlE '/^\s*(PERF_\w+)/ && say "$1 = C.$1"' /usr/include/linux/perf_event.h
 const (
-	PERF_TYPE_HARDWARE   = C.PERF_TYPE_HARDWARE
-	PERF_TYPE_SOFTWARE   = C.PERF_TYPE_SOFTWARE
-	PERF_TYPE_TRACEPOINT = C.PERF_TYPE_TRACEPOINT
-	PERF_TYPE_HW_CACHE   = C.PERF_TYPE_HW_CACHE
-	PERF_TYPE_RAW        = C.PERF_TYPE_RAW
-	PERF_TYPE_BREAKPOINT = C.PERF_TYPE_BREAKPOINT
-
+	PERF_FLAG_FD_CLOEXEF_TYPE_HARDWARE    = C.PERF_TYPE_HARDWARE
+	PERF_TYPE_SOFTWARE                    = C.PERF_TYPE_SOFTWARE
+	PERF_TYPE_TRACEPOINT                  = C.PERF_TYPE_TRACEPOINT
+	PERF_TYPE_HW_CACHE                    = C.PERF_TYPE_HW_CACHE
+	PERF_TYPE_RAW                         = C.PERF_TYPE_RAW
+	PERF_TYPE_BREAKPOINT                  = C.PERF_TYPE_BREAKPOINT
+	PERF_TYPE_MAX                         = C.PERF_TYPE_MAX
 	PERF_COUNT_HW_CPU_CYCLES              = C.PERF_COUNT_HW_CPU_CYCLES
 	PERF_COUNT_HW_INSTRUCTIONS            = C.PERF_COUNT_HW_INSTRUCTIONS
 	PERF_COUNT_HW_CACHE_REFERENCES        = C.PERF_COUNT_HW_CACHE_REFERENCES
@@ -1056,106 +1056,161 @@ const (
 	PERF_COUNT_HW_STALLED_CYCLES_FRONTEND = C.PERF_COUNT_HW_STALLED_CYCLES_FRONTEND
 	PERF_COUNT_HW_STALLED_CYCLES_BACKEND  = C.PERF_COUNT_HW_STALLED_CYCLES_BACKEND
 	PERF_COUNT_HW_REF_CPU_CYCLES          = C.PERF_COUNT_HW_REF_CPU_CYCLES
-
-	PERF_COUNT_HW_CACHE_L1D  = C.PERF_COUNT_HW_CACHE_L1D
-	PERF_COUNT_HW_CACHE_L1I  = C.PERF_COUNT_HW_CACHE_L1I
-	PERF_COUNT_HW_CACHE_LL   = C.PERF_COUNT_HW_CACHE_LL
-	PERF_COUNT_HW_CACHE_DTLB = C.PERF_COUNT_HW_CACHE_DTLB
-	PERF_COUNT_HW_CACHE_ITLB = C.PERF_COUNT_HW_CACHE_ITLB
-	PERF_COUNT_HW_CACHE_BPU  = C.PERF_COUNT_HW_CACHE_BPU
-	PERF_COUNT_HW_CACHE_NODE = C.PERF_COUNT_HW_CACHE_NODE
-
-	PERF_COUNT_HW_CACHE_OP_READ     = C.PERF_COUNT_HW_CACHE_OP_READ
-	PERF_COUNT_HW_CACHE_OP_WRITE    = C.PERF_COUNT_HW_CACHE_OP_WRITE
-	PERF_COUNT_HW_CACHE_OP_PREFETCH = C.PERF_COUNT_HW_CACHE_OP_PREFETCH
-
-	PERF_COUNT_HW_CACHE_RESULT_ACCESS = C.PERF_COUNT_HW_CACHE_RESULT_ACCESS
-	PERF_COUNT_HW_CACHE_RESULT_MISS   = C.PERF_COUNT_HW_CACHE_RESULT_MISS
-
-	PERF_COUNT_SW_CPU_CLOCK        = C.PERF_COUNT_SW_CPU_CLOCK
-	PERF_COUNT_SW_TASK_CLOCK       = C.PERF_COUNT_SW_TASK_CLOCK
-	PERF_COUNT_SW_PAGE_FAULTS      = C.PERF_COUNT_SW_PAGE_FAULTS
-	PERF_COUNT_SW_CONTEXT_SWITCHES = C.PERF_COUNT_SW_CONTEXT_SWITCHES
-	PERF_COUNT_SW_CPU_MIGRATIONS   = C.PERF_COUNT_SW_CPU_MIGRATIONS
-	PERF_COUNT_SW_PAGE_FAULTS_MIN  = C.PERF_COUNT_SW_PAGE_FAULTS_MIN
-	PERF_COUNT_SW_PAGE_FAULTS_MAJ  = C.PERF_COUNT_SW_PAGE_FAULTS_MAJ
-	PERF_COUNT_SW_ALIGNMENT_FAULTS = C.PERF_COUNT_SW_ALIGNMENT_FAULTS
-	PERF_COUNT_SW_EMULATION_FAULTS = C.PERF_COUNT_SW_EMULATION_FAULTS
-	PERF_COUNT_SW_DUMMY            = C.PERF_COUNT_SW_DUMMY
-	PERF_COUNT_SW_BPF_OUTPUT       = C.PERF_COUNT_SW_BPF_OUTPUT
-
-	PERF_SAMPLE_IP           = C.PERF_SAMPLE_IP
-	PERF_SAMPLE_TID          = C.PERF_SAMPLE_TID
-	PERF_SAMPLE_TIME         = C.PERF_SAMPLE_TIME
-	PERF_SAMPLE_ADDR         = C.PERF_SAMPLE_ADDR
-	PERF_SAMPLE_READ         = C.PERF_SAMPLE_READ
-	PERF_SAMPLE_CALLCHAIN    = C.PERF_SAMPLE_CALLCHAIN
-	PERF_SAMPLE_ID           = C.PERF_SAMPLE_ID
-	PERF_SAMPLE_CPU          = C.PERF_SAMPLE_CPU
-	PERF_SAMPLE_PERIOD       = C.PERF_SAMPLE_PERIOD
-	PERF_SAMPLE_STREAM_ID    = C.PERF_SAMPLE_STREAM_ID
-	PERF_SAMPLE_RAW          = C.PERF_SAMPLE_RAW
-	PERF_SAMPLE_BRANCH_STACK = C.PERF_SAMPLE_BRANCH_STACK
-	PERF_SAMPLE_REGS_USER    = C.PERF_SAMPLE_REGS_USER
-	PERF_SAMPLE_STACK_USER   = C.PERF_SAMPLE_STACK_USER
-	PERF_SAMPLE_WEIGHT       = C.PERF_SAMPLE_WEIGHT
-	PERF_SAMPLE_DATA_SRC     = C.PERF_SAMPLE_DATA_SRC
-	PERF_SAMPLE_IDENTIFIER   = C.PERF_SAMPLE_IDENTIFIER
-	PERF_SAMPLE_TRANSACTION  = C.PERF_SAMPLE_TRANSACTION
-	PERF_SAMPLE_REGS_INTR    = C.PERF_SAMPLE_REGS_INTR
-
-	PERF_SAMPLE_BRANCH_USER       = C.PERF_SAMPLE_BRANCH_USER
-	PERF_SAMPLE_BRANCH_KERNEL     = C.PERF_SAMPLE_BRANCH_KERNEL
-	PERF_SAMPLE_BRANCH_HV         = C.PERF_SAMPLE_BRANCH_HV
-	PERF_SAMPLE_BRANCH_ANY        = C.PERF_SAMPLE_BRANCH_ANY
-	PERF_SAMPLE_BRANCH_ANY_CALL   = C.PERF_SAMPLE_BRANCH_ANY_CALL
-	PERF_SAMPLE_BRANCH_ANY_RETURN = C.PERF_SAMPLE_BRANCH_ANY_RETURN
-	PERF_SAMPLE_BRANCH_IND_CALL   = C.PERF_SAMPLE_BRANCH_IND_CALL
-	PERF_SAMPLE_BRANCH_ABORT_TX   = C.PERF_SAMPLE_BRANCH_ABORT_TX
-	PERF_SAMPLE_BRANCH_IN_TX      = C.PERF_SAMPLE_BRANCH_IN_TX
-	PERF_SAMPLE_BRANCH_NO_TX      = C.PERF_SAMPLE_BRANCH_NO_TX
-	PERF_SAMPLE_BRANCH_COND       = C.PERF_SAMPLE_BRANCH_COND
-	PERF_SAMPLE_BRANCH_CALL_STACK = C.PERF_SAMPLE_BRANCH_CALL_STACK
-	PERF_SAMPLE_BRANCH_IND_JUMP   = C.PERF_SAMPLE_BRANCH_IND_JUMP
-	PERF_SAMPLE_BRANCH_CALL       = C.PERF_SAMPLE_BRANCH_CALL
-	PERF_SAMPLE_BRANCH_NO_FLAGS   = C.PERF_SAMPLE_BRANCH_NO_FLAGS
-	PERF_SAMPLE_BRANCH_NO_CYCLES  = C.PERF_SAMPLE_BRANCH_NO_CYCLES
-	PERF_SAMPLE_BRANCH_TYPE_SAVE  = C.PERF_SAMPLE_BRANCH_TYPE_SAVE
-
-	PERF_FORMAT_TOTAL_TIME_ENABLED = C.PERF_FORMAT_TOTAL_TIME_ENABLED
-	PERF_FORMAT_TOTAL_TIME_RUNNING = C.PERF_FORMAT_TOTAL_TIME_RUNNING
-	PERF_FORMAT_ID                 = C.PERF_FORMAT_ID
-	PERF_FORMAT_GROUP              = C.PERF_FORMAT_GROUP
-
-	PERF_RECORD_MMAP            = C.PERF_RECORD_MMAP
-	PERF_RECORD_LOST            = C.PERF_RECORD_LOST
-	PERF_RECORD_COMM            = C.PERF_RECORD_COMM
-	PERF_RECORD_EXIT            = C.PERF_RECORD_EXIT
-	PERF_RECORD_THROTTLE        = C.PERF_RECORD_THROTTLE
-	PERF_RECORD_UNTHROTTLE      = C.PERF_RECORD_UNTHROTTLE
-	PERF_RECORD_FORK            = C.PERF_RECORD_FORK
-	PERF_RECORD_READ            = C.PERF_RECORD_READ
-	PERF_RECORD_SAMPLE          = C.PERF_RECORD_SAMPLE
-	PERF_RECORD_MMAP2           = C.PERF_RECORD_MMAP2
-	PERF_RECORD_AUX             = C.PERF_RECORD_AUX
-	PERF_RECORD_ITRACE_START    = C.PERF_RECORD_ITRACE_START
-	PERF_RECORD_LOST_SAMPLES    = C.PERF_RECORD_LOST_SAMPLES
-	PERF_RECORD_SWITCH          = C.PERF_RECORD_SWITCH
-	PERF_RECORD_SWITCH_CPU_WIDE = C.PERF_RECORD_SWITCH_CPU_WIDE
-	PERF_RECORD_NAMESPACES      = C.PERF_RECORD_NAMESPACES
-
-	PERF_CONTEXT_HV     = C.PERF_CONTEXT_HV
-	PERF_CONTEXT_KERNEL = C.PERF_CONTEXT_KERNEL
-	PERF_CONTEXT_USER   = C.PERF_CONTEXT_USER
-
-	PERF_CONTEXT_GUEST        = C.PERF_CONTEXT_GUEST
-	PERF_CONTEXT_GUEST_KERNEL = C.PERF_CONTEXT_GUEST_KERNEL
-	PERF_CONTEXT_GUEST_USER   = C.PERF_CONTEXT_GUEST_USER
-
-	PERF_FLAG_FD_NO_GROUP = C.PERF_FLAG_FD_NO_GROUP
-	PERF_FLAG_FD_OUTPUT   = C.PERF_FLAG_FD_OUTPUT
-	PERF_FLAG_PID_CGROUP  = C.PERF_FLAG_PID_CGROUP
-	PERF_FLAG_FD_CLOEXEC  = C.PERF_FLAG_FD_CLOEXEC
+	PERF_COUNT_HW_MAX                     = C.PERF_COUNT_HW_MAX
+	PERF_COUNT_HW_CACHE_L1D               = C.PERF_COUNT_HW_CACHE_L1D
+	PERF_COUNT_HW_CACHE_L1I               = C.PERF_COUNT_HW_CACHE_L1I
+	PERF_COUNT_HW_CACHE_LL                = C.PERF_COUNT_HW_CACHE_LL
+	PERF_COUNT_HW_CACHE_DTLB              = C.PERF_COUNT_HW_CACHE_DTLB
+	PERF_COUNT_HW_CACHE_ITLB              = C.PERF_COUNT_HW_CACHE_ITLB
+	PERF_COUNT_HW_CACHE_BPU               = C.PERF_COUNT_HW_CACHE_BPU
+	PERF_COUNT_HW_CACHE_NODE              = C.PERF_COUNT_HW_CACHE_NODE
+	PERF_COUNT_HW_CACHE_MAX               = C.PERF_COUNT_HW_CACHE_MAX
+	PERF_COUNT_HW_CACHE_OP_READ           = C.PERF_COUNT_HW_CACHE_OP_READ
+	PERF_COUNT_HW_CACHE_OP_WRITE          = C.PERF_COUNT_HW_CACHE_OP_WRITE
+	PERF_COUNT_HW_CACHE_OP_PREFETCH       = C.PERF_COUNT_HW_CACHE_OP_PREFETCH
+	PERF_COUNT_HW_CACHE_OP_MAX            = C.PERF_COUNT_HW_CACHE_OP_MAX
+	PERF_COUNT_HW_CACHE_RESULT_ACCESS     = C.PERF_COUNT_HW_CACHE_RESULT_ACCESS
+	PERF_COUNT_HW_CACHE_RESULT_MISS       = C.PERF_COUNT_HW_CACHE_RESULT_MISS
+	PERF_COUNT_HW_CACHE_RESULT_MAX        = C.PERF_COUNT_HW_CACHE_RESULT_MAX
+	PERF_COUNT_SW_CPU_CLOCK               = C.PERF_COUNT_SW_CPU_CLOCK
+	PERF_COUNT_SW_TASK_CLOCK              = C.PERF_COUNT_SW_TASK_CLOCK
+	PERF_COUNT_SW_PAGE_FAULTS             = C.PERF_COUNT_SW_PAGE_FAULTS
+	PERF_COUNT_SW_CONTEXT_SWITCHES        = C.PERF_COUNT_SW_CONTEXT_SWITCHES
+	PERF_COUNT_SW_CPU_MIGRATIONS          = C.PERF_COUNT_SW_CPU_MIGRATIONS
+	PERF_COUNT_SW_PAGE_FAULTS_MIN         = C.PERF_COUNT_SW_PAGE_FAULTS_MIN
+	PERF_COUNT_SW_PAGE_FAULTS_MAJ         = C.PERF_COUNT_SW_PAGE_FAULTS_MAJ
+	PERF_COUNT_SW_ALIGNMENT_FAULTS        = C.PERF_COUNT_SW_ALIGNMENT_FAULTS
+	PERF_COUNT_SW_EMULATION_FAULTS        = C.PERF_COUNT_SW_EMULATION_FAULTS
+	PERF_COUNT_SW_DUMMY                   = C.PERF_COUNT_SW_DUMMY
+	PERF_COUNT_SW_BPF_OUTPUT              = C.PERF_COUNT_SW_BPF_OUTPUT
+	PERF_COUNT_SW_MAX                     = C.PERF_COUNT_SW_MAX
+	PERF_SAMPLE_IP                        = C.PERF_SAMPLE_IP
+	PERF_SAMPLE_TID                       = C.PERF_SAMPLE_TID
+	PERF_SAMPLE_TIME                      = C.PERF_SAMPLE_TIME
+	PERF_SAMPLE_ADDR                      = C.PERF_SAMPLE_ADDR
+	PERF_SAMPLE_READ                      = C.PERF_SAMPLE_READ
+	PERF_SAMPLE_CALLCHAIN                 = C.PERF_SAMPLE_CALLCHAIN
+	PERF_SAMPLE_ID                        = C.PERF_SAMPLE_ID
+	PERF_SAMPLE_CPU                       = C.PERF_SAMPLE_CPU
+	PERF_SAMPLE_PERIOD                    = C.PERF_SAMPLE_PERIOD
+	PERF_SAMPLE_STREAM_ID                 = C.PERF_SAMPLE_STREAM_ID
+	PERF_SAMPLE_RAW                       = C.PERF_SAMPLE_RAW
+	PERF_SAMPLE_BRANCH_STACK              = C.PERF_SAMPLE_BRANCH_STACK
+	PERF_SAMPLE_REGS_USER                 = C.PERF_SAMPLE_REGS_USER
+	PERF_SAMPLE_STACK_USER                = C.PERF_SAMPLE_STACK_USER
+	PERF_SAMPLE_WEIGHT                    = C.PERF_SAMPLE_WEIGHT
+	PERF_SAMPLE_DATA_SRC                  = C.PERF_SAMPLE_DATA_SRC
+	PERF_SAMPLE_IDENTIFIER                = C.PERF_SAMPLE_IDENTIFIER
+	PERF_SAMPLE_TRANSACTION               = C.PERF_SAMPLE_TRANSACTION
+	PERF_SAMPLE_REGS_INTR                 = C.PERF_SAMPLE_REGS_INTR
+	PERF_SAMPLE_PHYS_ADDR                 = C.PERF_SAMPLE_PHYS_ADDR
+	PERF_SAMPLE_AUX                       = C.PERF_SAMPLE_AUX
+	PERF_SAMPLE_CGROUP                    = C.PERF_SAMPLE_CGROUP
+	PERF_SAMPLE_MAX                       = C.PERF_SAMPLE_MAX
+	PERF_SAMPLE_BRANCH_USER_SHIFT         = C.PERF_SAMPLE_BRANCH_USER_SHIFT
+	PERF_SAMPLE_BRANCH_KERNEL_SHIFT       = C.PERF_SAMPLE_BRANCH_KERNEL_SHIFT
+	PERF_SAMPLE_BRANCH_HV_SHIFT           = C.PERF_SAMPLE_BRANCH_HV_SHIFT
+	PERF_SAMPLE_BRANCH_ANY_SHIFT          = C.PERF_SAMPLE_BRANCH_ANY_SHIFT
+	PERF_SAMPLE_BRANCH_ANY_CALL_SHIFT     = C.PERF_SAMPLE_BRANCH_ANY_CALL_SHIFT
+	PERF_SAMPLE_BRANCH_ANY_RETURN_SHIFT   = C.PERF_SAMPLE_BRANCH_ANY_RETURN_SHIFT
+	PERF_SAMPLE_BRANCH_IND_CALL_SHIFT     = C.PERF_SAMPLE_BRANCH_IND_CALL_SHIFT
+	PERF_SAMPLE_BRANCH_ABORT_TX_SHIFT     = C.PERF_SAMPLE_BRANCH_ABORT_TX_SHIFT
+	PERF_SAMPLE_BRANCH_IN_TX_SHIFT        = C.PERF_SAMPLE_BRANCH_IN_TX_SHIFT
+	PERF_SAMPLE_BRANCH_NO_TX_SHIFT        = C.PERF_SAMPLE_BRANCH_NO_TX_SHIFT
+	PERF_SAMPLE_BRANCH_COND_SHIFT         = C.PERF_SAMPLE_BRANCH_COND_SHIFT
+	PERF_SAMPLE_BRANCH_CALL_STACK_SHIFT   = C.PERF_SAMPLE_BRANCH_CALL_STACK_SHIFT
+	PERF_SAMPLE_BRANCH_IND_JUMP_SHIFT     = C.PERF_SAMPLE_BRANCH_IND_JUMP_SHIFT
+	PERF_SAMPLE_BRANCH_CALL_SHIFT         = C.PERF_SAMPLE_BRANCH_CALL_SHIFT
+	PERF_SAMPLE_BRANCH_NO_FLAGS_SHIFT     = C.PERF_SAMPLE_BRANCH_NO_FLAGS_SHIFT
+	PERF_SAMPLE_BRANCH_NO_CYCLES_SHIFT    = C.PERF_SAMPLE_BRANCH_NO_CYCLES_SHIFT
+	PERF_SAMPLE_BRANCH_TYPE_SAVE_SHIFT    = C.PERF_SAMPLE_BRANCH_TYPE_SAVE_SHIFT
+	PERF_SAMPLE_BRANCH_HW_INDEX_SHIFT     = C.PERF_SAMPLE_BRANCH_HW_INDEX_SHIFT
+	PERF_SAMPLE_BRANCH_MAX_SHIFT          = C.PERF_SAMPLE_BRANCH_MAX_SHIFT
+	PERF_SAMPLE_BRANCH_USER               = C.PERF_SAMPLE_BRANCH_USER
+	PERF_SAMPLE_BRANCH_KERNEL             = C.PERF_SAMPLE_BRANCH_KERNEL
+	PERF_SAMPLE_BRANCH_HV                 = C.PERF_SAMPLE_BRANCH_HV
+	PERF_SAMPLE_BRANCH_ANY                = C.PERF_SAMPLE_BRANCH_ANY
+	PERF_SAMPLE_BRANCH_ANY_CALL           = C.PERF_SAMPLE_BRANCH_ANY_CALL
+	PERF_SAMPLE_BRANCH_ANY_RETURN         = C.PERF_SAMPLE_BRANCH_ANY_RETURN
+	PERF_SAMPLE_BRANCH_IND_CALL           = C.PERF_SAMPLE_BRANCH_IND_CALL
+	PERF_SAMPLE_BRANCH_ABORT_TX           = C.PERF_SAMPLE_BRANCH_ABORT_TX
+	PERF_SAMPLE_BRANCH_IN_TX              = C.PERF_SAMPLE_BRANCH_IN_TX
+	PERF_SAMPLE_BRANCH_NO_TX              = C.PERF_SAMPLE_BRANCH_NO_TX
+	PERF_SAMPLE_BRANCH_COND               = C.PERF_SAMPLE_BRANCH_COND
+	PERF_SAMPLE_BRANCH_CALL_STACK         = C.PERF_SAMPLE_BRANCH_CALL_STACK
+	PERF_SAMPLE_BRANCH_IND_JUMP           = C.PERF_SAMPLE_BRANCH_IND_JUMP
+	PERF_SAMPLE_BRANCH_CALL               = C.PERF_SAMPLE_BRANCH_CALL
+	PERF_SAMPLE_BRANCH_NO_FLAGS           = C.PERF_SAMPLE_BRANCH_NO_FLAGS
+	PERF_SAMPLE_BRANCH_NO_CYCLES          = C.PERF_SAMPLE_BRANCH_NO_CYCLES
+	PERF_SAMPLE_BRANCH_TYPE_SAVE          = C.PERF_SAMPLE_BRANCH_TYPE_SAVE
+	PERF_SAMPLE_BRANCH_HW_INDEX           = C.PERF_SAMPLE_BRANCH_HW_INDEX
+	PERF_SAMPLE_BRANCH_MAX                = C.PERF_SAMPLE_BRANCH_MAX
+	PERF_BR_UNKNOWN                       = C.PERF_BR_UNKNOWN
+	PERF_BR_COND                          = C.PERF_BR_COND
+	PERF_BR_UNCOND                        = C.PERF_BR_UNCOND
+	PERF_BR_IND                           = C.PERF_BR_IND
+	PERF_BR_CALL                          = C.PERF_BR_CALL
+	PERF_BR_IND_CALL                      = C.PERF_BR_IND_CALL
+	PERF_BR_RET                           = C.PERF_BR_RET
+	PERF_BR_SYSCALL                       = C.PERF_BR_SYSCALL
+	PERF_BR_SYSRET                        = C.PERF_BR_SYSRET
+	PERF_BR_COND_CALL                     = C.PERF_BR_COND_CALL
+	PERF_BR_COND_RET                      = C.PERF_BR_COND_RET
+	PERF_BR_MAX                           = C.PERF_BR_MAX
+	PERF_SAMPLE_REGS_ABI_NONE             = C.PERF_SAMPLE_REGS_ABI_NONE
+	PERF_SAMPLE_REGS_ABI_32               = C.PERF_SAMPLE_REGS_ABI_32
+	PERF_SAMPLE_REGS_ABI_64               = C.PERF_SAMPLE_REGS_ABI_64
+	PERF_TXN_ELISION                      = C.PERF_TXN_ELISION
+	PERF_TXN_TRANSACTION                  = C.PERF_TXN_TRANSACTION
+	PERF_TXN_SYNC                         = C.PERF_TXN_SYNC
+	PERF_TXN_ASYNC                        = C.PERF_TXN_ASYNC
+	PERF_TXN_RETRY                        = C.PERF_TXN_RETRY
+	PERF_TXN_CONFLICT                     = C.PERF_TXN_CONFLICT
+	PERF_TXN_CAPACITY_WRITE               = C.PERF_TXN_CAPACITY_WRITE
+	PERF_TXN_CAPACITY_READ                = C.PERF_TXN_CAPACITY_READ
+	PERF_TXN_MAX                          = C.PERF_TXN_MAX
+	PERF_TXN_ABORT_MASK                   = C.PERF_TXN_ABORT_MASK
+	PERF_TXN_ABORT_SHIFT                  = C.PERF_TXN_ABORT_SHIFT
+	PERF_FORMAT_TOTAL_TIME_ENABLED        = C.PERF_FORMAT_TOTAL_TIME_ENABLED
+	PERF_FORMAT_TOTAL_TIME_RUNNING        = C.PERF_FORMAT_TOTAL_TIME_RUNNING
+	PERF_FORMAT_ID                        = C.PERF_FORMAT_ID
+	PERF_FORMAT_GROUP                     = C.PERF_FORMAT_GROUP
+	PERF_FORMAT_MAX                       = C.PERF_FORMAT_MAX
+	PERF_IOC_FLAG_GROUP                   = C.PERF_IOC_FLAG_GROUP
+	PERF_RECORD_MMAP                      = C.PERF_RECORD_MMAP
+	PERF_RECORD_LOST                      = C.PERF_RECORD_LOST
+	PERF_RECORD_COMM                      = C.PERF_RECORD_COMM
+	PERF_RECORD_EXIT                      = C.PERF_RECORD_EXIT
+	PERF_RECORD_THROTTLE                  = C.PERF_RECORD_THROTTLE
+	PERF_RECORD_UNTHROTTLE                = C.PERF_RECORD_UNTHROTTLE
+	PERF_RECORD_FORK                      = C.PERF_RECORD_FORK
+	PERF_RECORD_READ                      = C.PERF_RECORD_READ
+	PERF_RECORD_SAMPLE                    = C.PERF_RECORD_SAMPLE
+	PERF_RECORD_MMAP2                     = C.PERF_RECORD_MMAP2
+	PERF_RECORD_AUX                       = C.PERF_RECORD_AUX
+	PERF_RECORD_ITRACE_START              = C.PERF_RECORD_ITRACE_START
+	PERF_RECORD_LOST_SAMPLES              = C.PERF_RECORD_LOST_SAMPLES
+	PERF_RECORD_SWITCH                    = C.PERF_RECORD_SWITCH
+	PERF_RECORD_SWITCH_CPU_WIDE           = C.PERF_RECORD_SWITCH_CPU_WIDE
+	PERF_RECORD_NAMESPACES                = C.PERF_RECORD_NAMESPACES
+	PERF_RECORD_KSYMBOL                   = C.PERF_RECORD_KSYMBOL
+	PERF_RECORD_BPF_EVENT                 = C.PERF_RECORD_BPF_EVENT
+	PERF_RECORD_CGROUP                    = C.PERF_RECORD_CGROUP
+	PERF_RECORD_MAX                       = C.PERF_RECORD_MAX
+	PERF_RECORD_KSYMBOL_TYPE_UNKNOWN      = C.PERF_RECORD_KSYMBOL_TYPE_UNKNOWN
+	PERF_RECORD_KSYMBOL_TYPE_BPF          = C.PERF_RECORD_KSYMBOL_TYPE_BPF
+	PERF_RECORD_KSYMBOL_TYPE_MAX          = C.PERF_RECORD_KSYMBOL_TYPE_MAX
+	PERF_BPF_EVENT_UNKNOWN                = C.PERF_BPF_EVENT_UNKNOWN
+	PERF_BPF_EVENT_PROG_LOAD              = C.PERF_BPF_EVENT_PROG_LOAD
+	PERF_BPF_EVENT_PROG_UNLOAD            = C.PERF_BPF_EVENT_PROG_UNLOAD
+	PERF_BPF_EVENT_MAX                    = C.PERF_BPF_EVENT_MAX
+	PERF_CONTEXT_HV                       = C.PERF_CONTEXT_HV
+	PERF_CONTEXT_KERNEL                   = C.PERF_CONTEXT_KERNEL
+	PERF_CONTEXT_USER                     = C.PERF_CONTEXT_USER
+	PERF_CONTEXT_GUEST                    = C.PERF_CONTEXT_GUEST
+	PERF_CONTEXT_GUEST_KERNEL             = C.PERF_CONTEXT_GUEST_KERNEL
+	PERF_CONTEXT_GUEST_USER               = C.PERF_CONTEXT_GUEST_USER
+	PERF_CONTEXT_MAX                      = C.PERF_CONTEXT_MAX
 )
 
 // Platform ABI and calling convention

commit ac719f4daadf9a497a732e2461f70b8720a0287c

unix: fix typo in PERF_TYPE_HARDWARE introduced in CL 259477
Change-Id: Iee2a067673309abc6b80d3cc96ac49d9d6b52865
Reviewed-on: https://go-review.googlesource.com/c/sys/+/259799
Trust: Matt Layher <mdlayher@gmail.com>
Run-TryBot: Matt Layher <mdlayher@gmail.com>
TryBot-Result: Go Bot <gobot@golang.org>
Reviewed-by: Bryan C. Mills <bcmills@google.com>

unix/linux/types.go
@@ -1039,7 +1039,7 @@ const (
 // generated by:
 // perl -nlE '/^\s*(PERF_\w+)/ && say "$1 = C.$1"' /usr/include/linux/perf_event.h
 const (
-	PERF_FLAG_FD_CLOEXEF_TYPE_HARDWARE    = C.PERF_TYPE_HARDWARE
+	PERF_TYPE_HARDWARE                    = C.PERF_TYPE_HARDWARE
 	PERF_TYPE_SOFTWARE                    = C.PERF_TYPE_SOFTWARE
 	PERF_TYPE_TRACEPOINT                  = C.PERF_TYPE_TRACEPOINT
 	PERF_TYPE_HW_CACHE                    = C.PERF_TYPE_HW_CACHE

unix/ztypes_linux.go
@@ -988,7 +988,7 @@ const (
 )
 
 const (
-	PERF_FLAG_FD_CLOEXEF_TYPE_HARDWARE    = 0x0
+	PERF_TYPE_HARDWARE                    = 0x0
 	PERF_TYPE_SOFTWARE                    = 0x1
 	PERF_TYPE_TRACEPOINT                  = 0x2
 	PERF_TYPE_HW_CACHE                    = 0x3