4bcb84eeeb78d47aae60461dac79349e427b5349
Tobias Klauser <tobias.klauser@gmail.com>
2021-01-13 17:44:44 +0000

    Revert "unix: add Pipe2 on netbsd"
    This reverts CL 283593.
    
    Reason for revert: breaks DragonflyBSD builders and was submitted during code freeze.
    
    Change-Id: I784f58ae56f7259d5176921d7a61070e0ac7cb52
    Reviewed-on: https://go-review.googlesource.com/c/sys/+/283597
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

--- unix/syscall_solaris_test.go
+++ unix/syscall_solaris_test.go
@@ -13,6 +13,48 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+func TestPipe2(t *testing.T) {
+	const s = "hello"
+	var pipes [2]int
+	err := unix.Pipe2(pipes[:], 0)
+	if err != nil {
+		t.Fatalf("pipe2: %v", err)
+	}
+	r := pipes[0]
+	w := pipes[1]
+	go func() {
+		n, err := unix.Write(w, []byte(s))
+		if err != nil {
+			t.Errorf("bad write: %v", err)
+			return
+		}
+		if n != len(s) {
+			t.Errorf("bad write count: %d", n)
+			return
+		}
+		err = unix.Close(w)
+		if err != nil {
+			t.Errorf("bad close: %v", err)
+			return
+		}
+	}()
+	var buf [10 + len(s)]byte
+	n, err := unix.Read(r, buf[:])
+	if err != nil {
+		t.Fatalf("bad read: %v", err)
+	}
+	if n != len(s) {
+		t.Fatalf("bad read count: %d", n)
+	}
+	if string(buf[:n]) != s {
+		t.Fatalf("bad contents: %s", string(buf[:n]))
+	}
+	err = unix.Close(r)
+	if err != nil {
+		t.Fatalf("bad close: %v", err)
+	}
+}
+
 func TestStatvfs(t *testing.T) {
 	if err := unix.Statvfs("", nil); err == nil {
 		t.Fatal(`Statvfs("") expected failure`)
