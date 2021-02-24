4ada9433c6eaf608b86cfc89aaefd8cc8569189e
Tobias Klauser <tklauser@distanz.ch>
2021-02-24 15:08:13 +0100

    unix: test nonblocking and close-on-exec behavior of Pipe2
    Following TestNonblockingPipe in package runtime.
    
    Change-Id: Ifacec3ea6ce372a891273e3ec9fa4194f7d17619
    Reviewed-on: https://go-review.googlesource.com/c/sys/+/295869
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- unix/pipe2_test.go
+++ unix/pipe2_test.go
@@ -54,3 +54,42 @@ func TestPipe2(t *testing.T) {
 		t.Fatalf("bad close: %v", err)
 	}
 }
+
+func checkNonblocking(t *testing.T, fd int, name string) {
+	t.Helper()
+	flags, err := unix.FcntlInt(uintptr(fd), unix.F_GETFL, 0)
+	if err != nil {
+		t.Errorf("fcntl(%s, F_GETFL) failed: %v", name, err)
+	} else if flags&unix.O_NONBLOCK == 0 {
+		t.Errorf("O_NONBLOCK not set in %s flags %#x", name, flags)
+	}
+}
+
+func checkCloseonexec(t *testing.T, fd int, name string) {
+	t.Helper()
+	flags, err := unix.FcntlInt(uintptr(fd), unix.F_GETFD, 0)
+	if err != nil {
+		t.Errorf("fcntl(%s, F_GETFD) failed: %v", name, err)
+	} else if flags&unix.FD_CLOEXEC == 0 {
+		t.Errorf("FD_CLOEXEC not set in %s flags %#x", name, flags)
+	}
+}
+
+func TestNonblockingPipe2(t *testing.T) {
+	var pipes [2]int
+	err := unix.Pipe2(pipes[:], unix.O_NONBLOCK|unix.O_CLOEXEC)
+	if err != nil {
+		t.Fatalf("pipe2: %v", err)
+	}
+	r := pipes[0]
+	w := pipes[1]
+	defer func() {
+		unix.Close(r)
+		unix.Close(w)
+	}()
+
+	checkNonblocking(t, r, "reader")
+	checkCloseonexec(t, r, "reader")
+	checkNonblocking(t, w, "writer")
+	checkCloseonexec(t, w, "writer")
+}
