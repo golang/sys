af8b64212486582a9c87128a5289fc4ad7b9c922
Tobias Klauser <tklauser@distanz.ch>
2021-12-09 23:31:52 +0100

    unix: use pipe2 syscall in Pipe on NetBSD
    The pipe2 syscall is part of NetBSD since version 6.0 and thus exists in
    all officially supported versions (6.0 through 6.1 and 7.0+).
    
    Follows CL 94035 which did the same for package syscall.
    
    Change-Id: Icfa7dfb13788c9a7425c641d0233b109e8c79e7f
    Reviewed-on: https://go-review.googlesource.com/c/sys/+/370794
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Benny Siegert <bsiegert@gmail.com>
    Trust: Benny Siegert <bsiegert@gmail.com>

--- unix/syscall_netbsd.go
+++ unix/syscall_netbsd.go
@@ -110,14 +110,8 @@ func direntNamlen(buf []byte) (uint64, bool) {
 	return readInt(buf, unsafe.Offsetof(Dirent{}.Namlen), unsafe.Sizeof(Dirent{}.Namlen))
 }
 
-//sysnb	pipe() (fd1 int, fd2 int, err error)
-
 func Pipe(p []int) (err error) {
-	if len(p) != 2 {
-		return EINVAL
-	}
-	p[0], p[1], err = pipe()
-	return
+	return Pipe2(p, 0)
 }
 
 //sysnb	pipe2(p *[2]_C_int, flags int) (err error)
