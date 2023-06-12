67f5cae6badaf3e6b1884acb474f9795f8919fe0
Anton Kuklin <anton.a.kuklin@gmail.com>
2023-06-12 19:06:36 +0100

    update script to generate mremap flags

---
+++ unix/mremap_linux_test.go
@@ -0,0 +1,31 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build linux
+// +build linux
+
+package unix_test
+
+import (
+	"golang.org/x/sys/unix"
+	"testing"
+)
+
+func TestMremap(t *testing.T) {
+
+}
+
+func TestMremap2(t *testing.T) {
+	b, err := unix.Mmap(-1, 0, unix.Getpagesize()*2, unix.PROT_NONE, unix.MAP_ANON|unix.MAP_PRIVATE)
+	if err != nil {
+		t.Fatalf("Mmap: %v", err)
+	}
+
+	b[0] = 42
+	if err := unix.Msync(b, unix.MS_SYNC); err != nil {
+		t.Fatalf("Msync: %v", err)
+	}
+
+	newB, err := unix.Mremap2(b, unix.Getpagesize())
+}
