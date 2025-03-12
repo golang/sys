7401cce31392295f531e1d0fcc2f01d782353300
Guoqi Chen <chenguoqi@loongson.cn>
2025-03-12 10:18:00 +0800

    cpu: replace specific instructions with WORD in the function get_cpucfg on loong64
    The CPUCFG instruction on loong64 was introduced in Go1.24, which caused
    compilation errors when using this feature in Go1.23 and earlier versions.
    
    Change-Id: I68891bbfc527194ecdafebac3398e700bfb53c2f
    Reviewed-on: https://go-review.googlesource.com/c/sys/+/656915
    Reviewed-by: Meidan Li <limeidan@loongson.cn>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: David Chase <drchase@google.com>
    Reviewed-by: Junyang Shao <shaojunyang@google.com>

--- cpu/cpu_loong64.s
+++ cpu/cpu_loong64.s
@@ -7,6 +7,7 @@
 // func get_cpucfg(reg uint32) uint32
 TEXT ·get_cpucfg(SB), NOSPLIT|NOFRAME, $0
 	MOVW	reg+0(FP), R5
-	CPUCFG	R5, R4
+	// CPUCFG R5, R4 = 0x00006ca4
+	WORD	$0x00006ca4
 	MOVW	R4, ret+8(FP)
 	RET
