942780bbc19517df4948a6dbc8c33d2228e5f905
Egon Elbre <egonelbre@gmail.com>
2026-01-17 15:34:41 +0200

    cpu: darwin/arm64 feature detection
    The ARM64 flags were not being correctly set for Mac M series laptops.
    
    Fixes golang/go#43046
    
    Change-Id: I1d884dccd3f3a0a010ad7babbab26b73731ab583
    Reviewed-on: https://go-review.googlesource.com/c/sys/+/737260
    Auto-Submit: Keith Randall <khr@golang.org>
    Reviewed-by: Keith Randall <khr@google.com>
    Reviewed-by: Keith Randall <khr@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: David Chase <drchase@google.com>

--- cpu/cpu_other_arm64.go
+++ cpu/cpu_other_arm64.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build !linux && !netbsd && !openbsd && !windows && !darwin && arm64
+//go:build !darwin && !linux && !netbsd && !openbsd && !windows && arm64
 
 package cpu
 
